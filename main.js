6186cf7e1f47851cd939b63bc19a66e6c9504506
kritomas <Unknown>
2024-11-16 18:42:45 +0100

    Initial socket IO.

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('public'));
+
 io.on("connection", (socket) =>
 {
 	console.log("User connected");
