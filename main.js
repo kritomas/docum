f1a43401094e72ab8c2a8fe4bceb08a3696d6249
kritomas <Unknown>
2024-11-16 20:16:57 +0100

    Added initial document joining.

--- main.js
+++ main.js
@@ -8,11 +8,21 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+let users = {};
+let text = "BRYNDZOVÉ HALUŠKY";
+
 app.use(express.static('public'));
 
 io.on("connection", (socket) =>
 {
-	console.log("User connected");
+	console.log("User connected: " + socket.id);
+
+	socket.on("COMM_JOIN", (username) =>
+	{
+		users[socket.id] = username;
+		socket.emit("COMM_DOCUMENT_SET", text);
+		io.emit("COMM_USERS", users);
+	});
 });
 
 server.listen(PORT);
--- public/socket.js
+++ public/socket.js
@@ -1,6 +1,16 @@
 const socket = io();
+const editorContainer = document.getElementById('editor-container');
+const status = document.getElementById('status');
+let username;
 
 socket.on("connect", () =>
 {
 	console.log("Connected to server");
 })
+
+socket.on("COMM_DOCUMENT_SET", (incoming) =>
+{
+	editorContainer.innerText = incoming;
+})
+
+socket.emit("COMM_JOIN", "kritomas");
